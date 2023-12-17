2ae363c789c2438179f0c9237411ef7200b50ca7
fdai7060 <caroline-elisabeth.schweizer@informatik.hs-fulda.de>
2023-12-17 16:37:48 +0100

    Loop ends correctly

--- src/neopixel.hpp
+++ src/neopixel.hpp
@@ -2,9 +2,17 @@
 #define NEOPIXEL_H
 
 #include <Adafruit_NeoPixel.h>
+#include <math.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
+
+// Global variables
+extern uint8_t minValue;
+extern uint8_t maxValue;
+extern 
+
+// Define main colours
 
 // Declare the NeoPixel object globally
 extern Adafruit_NeoPixel strip;
@@ -26,5 +34,7 @@ void reverseRainbow(uint8_t wait);
 void theaterChase(uint32_t c, uint8_t wait);
 void increaseBrightness(uint8_t brightness);
 void decreaseBrightness(uint8_t brightness);
+void colorTransition(int startR, int startG, int startB, int endR, int endG, int endB, int duration);
+void colorTransitionReverse(int startR, int startG, int startB, int endR, int endG, int endB, int duration);
 
 #endif // NEOPIXEL_H
