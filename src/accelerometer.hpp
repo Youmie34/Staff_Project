27aad2a0639a97c87a1cd8cce2cddf1e3a8e076c
fdai7060 <caroline-elisabeth.schweizer@informatik.hs-fulda.de>
2023-12-18 16:28:06 +0100

    Accelerometer SPI

--- src/accelerometer.hpp
+++ src/accelerometer.hpp
@@ -3,9 +3,17 @@
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 
+//SPI
+extern const int LIS3DH_SCLK;
+extern const int LIS3DH_MISO;
+extern const int LIS3DH_MOSI;
+extern const int LIS3DH_CS;
+
+//I2C
 extern const int SDA_PIN;
 extern const int SCL_PIN;
 
 extern Adafruit_LIS3DH lis;
 
 void accStart();
+void measure();
