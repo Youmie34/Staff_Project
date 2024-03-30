b8d66720e0d645f3a7988e0f0cbb517225490f30
fdai7060 <caroline-elisabeth.schweizer@informatik.hs-fulda.de>
2024-03-30 12:09:50 +0100

    connection to SD-card works

--- src/memory.hpp
+++ src/memory.hpp
@@ -13,17 +13,8 @@ extern int miso;
 extern int mosi;
 extern int cs;
 
-// extern uint8_t cardType;
-
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels);
-void createDir(fs::FS &fs, const char *path);
-void removeDir(fs::FS &fs, const char *path);
 void readFile(fs::FS &fs, const char *path);
-void writeFile(fs::FS &fs, const char *path, const char *message);
-void appendFile(fs::FS &fs, const char *path, const char *message);
-void renameFile(fs::FS &fs, const char *path1, const char *path2);
-void deleteFile(fs::FS &fs, const char *path);
-void testFileIO(fs::FS &fs, const char *path);
 void setupMemory();
 
 #endif // MEMORY_HPP
