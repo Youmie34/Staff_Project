ca3302be9cc2ceda1ca85f955fd489e7aba63754
fdai7060 <caroline-elisabeth.schweizer@informatik.hs-fulda.de>
2024-08-30 11:49:19 +0200

    distance works with staff

--- src/distance.hpp
+++ src/distance.hpp
@@ -3,11 +3,11 @@
 #ifndef DISTANCE_H
 #define DISTANCE_H
 
-const int echoPin = 16; // Echo Pin
-const int trigPin = 17; // Trigger Pin
+const int echoPin = 16; // Echo Pin (green)
+const int trigPin = 17; // Trigger Pin (yellow)
 const int LEDPin = 5;   // Pin der LED
 
-const int minimumRange = 4;    // Minimale Reichweite
+const int minimumRange = 8; // Minimale Reichweite
 
 void ultrasonic();
 
